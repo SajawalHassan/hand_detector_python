3d263ce1ac263bc297018bba49c0cff836a38e90
SajawalHassan <scientificninja1f12@gmail.com>
2021-10-12 15:16:36 +0500

    Show video

---
+++ main.py
@@ -0,0 +1,21 @@
+import cv2 as cv
+
+# Capturing vid (cange filename to 0 if need webcam)
+capture = cv.VideoCapture("videos/hand_vid_test.3gp")
+
+while True:
+    # Reading currunt frame
+    succses, frame = capture.read()
+
+    # If can't read currunt frame, break loop
+    if not succses:
+        break
+
+    cv.imshow("Video", frame)
+    key = cv.waitKey(1)
+
+    if key==27:
+        break # If key is pressed, break loop
+
+capture.release()
+cv.destroyAllWindows()
