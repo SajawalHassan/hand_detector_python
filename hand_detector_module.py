54f5b5d1934a108050ee80663cdac23a3ed6cc70
SajawalHassan <scientificninja1f12@gmail.com>
2021-10-17 18:32:25 +0500

    Fix breaking issue in module

--- hand_detector_module.py
+++ hand_detector_module.py
@@ -2,104 +2,40 @@ import cv2 as cv
 import mediapipe as mp
 import time
 
-class PoseTracker():
-    def __init__(self, static_img=False, model_comp=1, landmarks=True, enable_segmentation=False,
-     segmentation=True, detection_con=0.5, tracking_con=0.5):
+from mediapipe.python.solutions.hands_connections import HAND_CONNECTIONS
 
+class HandDetector():
+    def __init__(self, static_img=False, max_num_hands=2, detection_con=0.5, tracking_con=0.5):
         self.static_img = static_img
-        self.model_comp = model_comp
-        self.landmarks = landmarks
-        self.enable_segmentation = enable_segmentation
-        self.segmentation = segmentation
+        self.max_num_hands = max_num_hands
         self.detection_con = detection_con
         self.tracking_con = tracking_con
 
-        self.mpPose = mp.solutions.pose
-        self.pose = self.mpPose.Pose()
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(static_img, max_num_hands, detection_con, tracking_con)
         self.mpDraw = mp.solutions.drawing_utils
 
-    def trackPose(self, img, draw=True):
-        # Convert to rgb
-        imgRgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    def detectHands(self,frame):
+        imgRGB = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
 
-        # Process results
-        self.results = self.pose.process(imgRgb)
+        if self.results.multi_hand_landmarks:
+            for self.hand in self.results.multi_hand_landmarks:
+                self.mpDraw.draw_landmarks(frame, self.hand, HAND_CONNECTIONS)
 
-        if draw:
-            if self.results.pose_landmarks:
-                self.mpDraw.draw_landmarks(img, self.results.pose_landmarks, self.mpPose.POSE_CONNECTIONS)
-        else:
-            print(self.results.pose_landmarks)
+    def findPos(self, img, lmNo=0, draw=True):
 
-    def findPos(self, img, draw=True):
+        lmList = []
 
-        self.lmList = []
-
-        if self.results.pose_landmarks:
-            for id, lm in enumerate(self.results.pose_landmarks.landmark):
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[lmNo] # Getting lm index
+            
+            for id, lm in enumerate(myHand.landmark):
                 h, w, c = img.shape # Get w, h
                 cx, cy = int(lm.x * w), int(lm.y * h) # Convert w, h to pixels
-                self.lmList.append([id, cx, cy]) # Add updates values to lmList
+                lmList.append([id, cx, cy]) # Add updates values to lmList
 
                 if draw:
                     cv.circle(img, (cx, cy), 10, (0, 255, 0), cv.FILLED)
 
-        return self.lmList
-
-    def findAngle(self, img, p1, p2, p3, draw=True):
-
-        x1, y1 = self.lmList[p1][1:]
-        x2, y2 = self.lmList[p2][1:]
-        x3, y3 = self.lmList[p3][1:]
-
-        if draw:
-            cv.line(img, (x1, y1), (x2, y2), (255,255,255), 3)
-            cv.line(img, (x2, y2), (x3, y3), (255,255,255), 3)
-            cv.circle(img, (x1, y1), 10, (0,0,255), -1)
-            cv.circle(img, (x1, y1), 15, (0,0,255), 2)
-            cv.circle(img, (x2, y2), 10, (0,0,255), -1)
-            cv.circle(img, (x2, y2), 15, (0,0,255), 2)
-            cv.circle(img, (x3, y3), 10, (0,0,255), -1)
-            cv.circle(img, (x3, y3), 15, (0,0,255), 2)
-
-        
-
-# FOR TESTING PURPOSES!
-def main():
-    # Capturing vid (cange filename to 0 if need webcam)
-    capture = cv.VideoCapture("videos/vid_test_body.3gp")
-
-    pTime = 0
-
-    while True:
-        # Reading currunt frame
-        success, img = capture.read()
-
-        # If can't read currunt frame, break loop
-        if not success:
-            break
-
-        detector = PoseTracker()
-
-        detector.trackPose(img)
-
-        # Calculate fps
-        Ctime = time.time()
-        fps = 1/(Ctime - pTime)
-        pTime = Ctime
-
-        # Display fps
-        cv.putText(img, str(int(fps)), (10, 70), cv.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 2)
-
-        cv.imshow("Video", img)
-        key = cv.waitKey(1)
-
-        if key==27:
-            break # If key is pressed, break loop
-
-    capture.release()
-    cv.destroyAllWindows()
-
-
-if __name__ == "__main__":
-    main()
+        return lmList
